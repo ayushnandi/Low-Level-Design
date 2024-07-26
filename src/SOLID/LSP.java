e20a88a73c8ec98eca825ac3c9efc626a2f5cb08
unknown <ayushnandi>
2024-07-26 15:06:32 +0530

    Liskov Substitution Principle

--- src/SOLID/LSP.java
+++ src/SOLID/LSP.java
@@ -1,5 +1,28 @@
 package SOLID;
 
-public class LSP {
-
+class Bird {
+    public void fly() {
+        System.out.println("Bird is flying");
+    }
 }
+
+class Sparrow extends Bird {
+    // Inherits fly() method from Bird
+}
+
+class Penguin extends Bird {
+    public void fly() {
+        throw new UnsupportedOperationException("Penguins can't fly");
+    }
+}
+
+public class LSP {
+    public static void main(String[] args) {
+        Bird sparrow = new Sparrow();
+        Bird penguin = new Penguin();
+
+        sparrow.fly();  // This works fine
+        penguin.fly();  // This will throw an exception
+    }
+}
+
